15b230c16db06e4f995c0b08edc1621a45f3fd0a
GuiiizeeraSouza <guilhermefsouzaaa@gmail.com>
2024-06-17 03:22:33 -0300

    projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
+import "./styles/Initial.css";
+import "./styles/skils.css"
+import "./styles/about.css"
+import "./styles/PageForms.css"
+import "./components/Forms/Forms.css"
+import "./components/Anchor/Anchor.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='!scroll-smooth'>
       <body className={inter.className}>{children}</body>
     </html>
   );
